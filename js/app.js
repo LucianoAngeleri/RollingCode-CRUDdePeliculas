4b093800894e9ef1adafa51db996c9413233222e
Luciano Angeleri <lcnang@gmail.com>
2023-04-20 17:28:18 -0300

    Leemos el local storage para obtener el tema configurado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let temaConfigurado = JSON.parse(localStorage.getItem("theme")) || "dark";
+cambiarTema(temaConfigurado)
 let btnThemeDark = document.getElementById("btnThemeDark")
 let btnThemeLight = document.getElementById("btnThemeLight")
 
@@ -6,4 +8,5 @@ btnThemeLight.addEventListener("click", () => cambiarTema("light"))
 
 function cambiarTema(tema) {
     document.querySelector("html").setAttribute("data-bs-theme",tema)
+    localStorage.setItem("theme", JSON.stringify(tema))
 }
