f64fd5ebb977d520df716b1e2982aa6792468976
Luciano Angeleri <lcnang@gmail.com>
2023-04-20 18:38:42 -0300

    Se agregó el evento submit para crear una pelicula

---
+++ js/administrador.js
@@ -0,0 +1,26 @@
+import Pelicula from "./classPelicula.js";
+
+let formularioPelicula = document.getElementById("formPelicula")
+
+formularioPelicula.addEventListener("submit",prepararFromularioPelicula)
+
+  function prepararFromularioPelicula(e){
+    e.preventDefault();
+    console.log("En el evento submit")
+    crearPelicula();
+  }
+  function crearPelicula(){
+    const peliculaEjemplo = new Pelicula(
+      "0001",
+      "El Padrino",
+      "La familia Corleone es una de las más poderosas de Nueva York en los años 40.",
+      "https://imagenes.psicologiaymente.com/wp-content/uploads/2021/02/el-padrino-1.png",
+      "Drama/Crimen",
+      1972,
+      "2h 55min",
+        "Estados Unidos",
+        ["Marlon Brando", "Al Pacino", "James Caan"]
+      );
+    console.log(peliculaEjemplo)
+  }
+  
---
+++ js/classPelicula.js
@@ -0,0 +1,95 @@
+export default
+    class Pelicula {
+        #codigo;
+        #titulo;
+        #descripcion;
+        #imagen;
+        #genero;
+        #anio;
+        #duracion;
+        #pais;
+        #reparto;
+        constructor(titulo, descripcion, imagen, genero, anio, duracion, pais, reparto) {
+          this.#codigo = 0;
+          this.#titulo = titulo;
+          this.#descripcion = descripcion;
+          this.#imagen = imagen;
+          this.#genero = genero;
+          this.#anio = anio;
+          this.#duracion = duracion;
+          this.#pais = pais;
+          this.#reparto = reparto;
+        }
+        get codigo() {
+          return this.#codigo;
+        }
+      
+        set codigo(codigo) {
+          this.#codigo = codigo;
+        }
+      
+        get titulo() {
+          return this.#titulo;
+        }
+      
+        set titulo(titulo) {
+          this.#titulo = titulo;
+        }
+      
+        get descripcion() {
+          return this.#descripcion;
+        }
+      
+        set descripcion(descripcion) {
+          this.#descripcion = descripcion;
+        }
+      
+        get imagen() {
+          return this.#imagen;
+        }
+      
+        set imagen(imagen) {
+          this.#imagen = imagen;
+        }
+      
+        get genero() {
+          return this.#genero;
+        }
+      
+        set genero(genero) {
+          this.#genero = genero;
+        }
+      
+        get anio() {
+          return this.#anio;
+        }
+      
+        set anio(anio) {
+          this.#anio = anio;
+        }
+      
+        get duracion() {
+          return this.#duracion;
+        }
+      
+        set duracion(duracion) {
+          this.#duracion = duracion;
+        }
+      
+        get pais() {
+          return this.#pais;
+        }
+      
+        set pais(pais) {
+          this.#pais = pais;
+        }
+      
+        get reparto() {
+          return this.#reparto;
+        }
+      
+        set reparto(reparto) {
+          this.#reparto = reparto;
+        }
+      }
+
