f9578ac2aa0ed437350bc886a83fa61afbf0fc8d
Luciano Angeleri <lcnang@gmail.com>
2023-04-27 05:22:10 -0300

    Se agregó la funcionalidad para cambiar el icono del tema seleccionado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 let temaConfigurado = JSON.parse(localStorage.getItem("theme")) || "dark";
 cambiarTema(temaConfigurado)
+cambiarIconoTema(temaConfigurado)
+console.log(themeIcon)
 let btnThemeDark = document.getElementById("btnThemeDark")
 let btnThemeLight = document.getElementById("btnThemeLight")
 
@@ -8,5 +10,15 @@ btnThemeLight.addEventListener("click", () => cambiarTema("light"))
 
 function cambiarTema(tema) {
     document.querySelector("html").setAttribute("data-bs-theme",tema)
+    cambiarIconoTema(tema)
     localStorage.setItem("theme", JSON.stringify(tema))
+}
+function cambiarIconoTema(tema) {
+    let themeIcon = document.getElementById("themeIcon")
+    if (tema === "light") {
+        themeIcon.className = "bi bi-moon-stars pe-1"
+        
+    }else{
+        themeIcon.className = "bi bi-moon-stars-fill pe-1"
+    }
 }
